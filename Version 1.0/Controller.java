85cc1bb291819d12f01541a1ca2441016fa495ef
unknown <stephanie.phung1@uoit.net>
2019-03-08 22:00:23 -0500

    Added comments, updated version to 1.1

--- Version 1.0/Controller.java
+++ Version 1.0/Controller.java
@@ -3,12 +3,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 /**
- * Write a description of class main here.
+ * Runs the main program logic of the back end.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Stephanie Phung
+ * @author
+ * @version 1.0
  */
 public class Controller{
+    // Private class variables for the Controller class.
     private String userFile;
     private String ticketFile;
     private ArrayList<User> users;
@@ -21,68 +23,126 @@ public class Controller{
         for (User u : users) {
             System.out.println(u.toTRN());
         }
+        
+        ArrayList<Event> events = parseEvents("available_tickets.data");
+        System.out.println("Saved:");
+        for (Event e : events) {
+            System.out.println(e.toTRN());
+        }
     }
 
+    /**
+     * Parses the current user accounts file and returns an ArrayList
+     * containing all the users.
+     *
+     * @param fileName file name of file to parse.
+     * @return ArrayList<User> contains all the users in the file.
+     */
     public static ArrayList<User> parseUsers(String fileName) {
         ArrayList<User> users = new ArrayList();
 
+        // Try opening the file.
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
+            // While the file has a next line, create a new user.
             while ((line = br.readLine()) != null) {
-                if (!line.equals("END")) {
-                    User user = new User(line);
-                    users.add(user);
+                User user = new User(line);
+                // If the username is valid, then add it to the list.
+                if (!(user.getName()).equals(user.KEY_END)) {
+                  users.add(user);
                 }
             }
-        } catch (IOException e) {
+            br.close();
+        } catch (IOException e) { // Print error if exists.
             e.printStackTrace();
         }
 
+        // Return the populated list.
         return users;
     }
 
-    public static ArrayList<Event> parseEvent(String fileName) {
+    /**
+     * Parses the tickets file and returns an ArrayList
+     * containing all the events.
+     *
+     * @param fileName file name of file to parse.
+     * @return ArrayList<Event> contains all the events found in the file.
+     */
+    public static ArrayList<Event> parseEvents(String fileName) {
         ArrayList<Event> events = new ArrayList();
 
+        // Try opening the file.
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
+            // While the file has a next line, create a new event.
             while ((line = br.readLine()) != null) {
-                if (!line.equals("END")) {
-                    Event event = new Event(line);
-                    events.add(event);
+                Event event = new Event(line);
+                // If the event name is valid, then add it to the list.
+                if (!(event.getName()).equals(event.KEY_END)) {
+                  events.add(event);
                 }
             }
-        } catch (IOException e) {
+            br.close();
+        } catch (IOException e) { // Print error if exists.
             e.printStackTrace();
         }
 
+        // Return the populated list.
         return events;
     }
 
+    /**
+     * Creates a user and adds it to the ArrayList.
+     *
+     * @param trn transaction string containing the user data.
+     * @param users the array list to add to.
+     */
     public static void createUser(String trn, ArrayList<User> users) {
         User user = new User(trn);
         users.add(user);
     }
 
+    /**
+     * Deletes the object that matches the given transaction string.
+     *
+     * @param trn transaction string containing data to delete.
+     * @param users search through this array list and delete the right object.
+     */
     public static void deleteUser(String trn, ArrayList<User> users) {
+        // Go through the list.
         for (int i = 0; i < users.size(); i++) {
+            // If the data matches, remove it.
             if (trn.equals(users.get(i).toTRN())) {
                 users.remove(i);
-                return;
+                return; // assume there's only one matching object
             }
         }
     }
 
+    /**
+     * Creates an event and adds it to the ArrayList.
+     *
+     * @param trn transaction string containing the event data.
+     * @param events the array list to add to.
+     */
     public static void createEvent(String trn, ArrayList<Event> events) {
         Event event = new Event(trn);
         events.add(event);
     }
 
+    /**
+     * Deletes the object that matches the given transaction string.
+     *
+     * @param trn transaction string containing data to delete.
+     * @param events search through this array list and delete the right object.
+     */
     public static void deleteEvent(String trn, ArrayList<Event> events) {
+        // Go through the list.
         for (int i = 0; i < events.size(); i++) {
+            // If the data matches, remove it.
             if (trn.equals(events.get(i).toTRN())) {
                 events.remove(i);
-                return;
+                return; // assume there's only one matching object
             }
         }
     }
--- Version 1.0/Handler.java
+++ Version 1.0/Handler.java
@@ -1,11 +1,13 @@
 
 /**
- * Abstract class Handler - write a description of the class here
+ * An abstract handler class containing useful functions to
+ * help manipulate data.
  *
- * @author (your name here)
- * @version (version number or date here)
+ * @author Stephanie Phung
+ * @version 1.1
  */
 public abstract class Handler {
+    // Constants from the supposed format of the data files.
     protected static final int LENGTH_USER = 15;
     protected static final int LENGTH_TYPE = 2;
     protected static final int LENGTH_CRED = 9;
@@ -20,27 +22,46 @@ public abstract class Handler {
     protected static final int KEY_PRICE = 6;
     protected static final String KEY_END = "END";
 
+    /**
+     * Returns a value with whitespace removed if necessary.
+     *
+     * @param trn transaction string of the data.
+     * @param removeWhitespace true if we need to remove whitespace.
+     * @param offset the beginning of the string value in the transaction string.
+     * @param end points to the end of the string value in the transaction string.
+     * @return String the trimmed value.
+     */
     protected String parseValue(String trn, boolean removeWhitespace, int offset, int end) {
         if (removeWhitespace) {
+            // Read from right to left.
             for (; end > offset + 1; end--) {
+                // If the character is not a space, then return it.
                 if (trn.charAt(end) != ' ') {
                     return trn.substring(offset, end + 1);
                 }
             }
-        } 
+        }
+        // Otherwise, return the value.
         return trn.substring(offset, end);
     }
 
+    /**
+     * Adds spaces or zeroes as padding to data, as necessary.
+     *
+     * @param dataType unique key of the type of data.
+     * @param value the value of the data to padd.
+     * @return String the padded data.
+     */
     protected String paddSpaces(int dataType, String value) {
-        if (value == "") {
+        if (value == "") { // if the value is empty, return it
             return value;
         }
 
-        int specifiedLength = 0;
-        String paddedValue = "";
-        String paddingChar = " ";
-        boolean padRight = true;
-        boolean numerical = false;
+        int specifiedLength = 0; // the correct length of the final value
+        String paddedValue = ""; // the end result to return
+        String paddingChar = " "; // default character to pad with is a space
+        boolean padRight = true; // pad from the right
+        boolean numerical = false; // true if value is numerical
 
         // Initializes the appropriate specified length.
         if (dataType == KEY_USER) {
@@ -81,6 +102,7 @@ public abstract class Handler {
             }
             paddedValue += value;
         }
+        // Return the final string result.
         return paddedValue;
     }
 }
